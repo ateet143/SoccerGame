2f2388f55761a62d4e4af1c02020bc22b4989563
ateet singh <ateet143@gmail.com>
2022-02-23 00:00:17 +0800

    Some changes

--- src/soccergame/SoccerGame.java
+++ src/soccergame/SoccerGame.java
@@ -7,6 +7,12 @@ public class SoccerGame {
         Scheduler s1 = new Scheduler(4);
         s1.addTeams();
         s1.startOfSeason();
+        
+        // this foreach loop will execute the scoreGoals method so that each games gets scoring.
+         for(Game game: s1.getGames()){
+             s1.scoringGoals(game.getTemperature(), game);
+         }
+         
         s1.displayAllGamesResult();
         s1.displayTeamResult();
     }
